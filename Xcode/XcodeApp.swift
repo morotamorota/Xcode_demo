6de19f434a1d5c26e3ce23a0f747349c84eaaad6
koichi <2020m086@kuas.ac.jp>
2023-09-20 14:28:12 +0900

    Initial Commit

---
+++ Xcode/XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeApp.swift
+//  Xcode
+//
+//  Created by 諸田紘一 on 2023-09-20.
+//
+
+import SwiftUI
+
+@main
+struct XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
